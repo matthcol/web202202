87169d94865299df845c4b527559c9bb31abecc4
matthcol <matthias.colin@gmail.com>
2022-02-26 12:33:59 +0100

    add Date examples

--- LearnJS/basics.js
+++ LearnJS/basics.js
@@ -36,6 +36,9 @@ function maj(texte) {
     return texte.toUpperCase()
 }
 
+// Tri ou recherche en fonction de la locale
+// https://developer.mozilla.org/fr/docs/Web/JavaScript/Reference/Global_Objects/Intl/Collator
+
 const compFr = new Intl.Collator('fr_FR').compare
 const compEs = new Intl.Collator('es_ES').compare
 
@@ -82,8 +85,23 @@ const ville_o = {
     pop: 77000
 }
 
-$("#panneau")
-S.fn.init [img#panneau]
-$("#panneau").css("display","none")
-S.fn.init [img#panneau]
-$("#panneau").show()
+// dates : type Date (année, mois, jour, heure, minute, seconde, millisec)
+// liste de librairies alternatives : https://www.skypack.dev/blog/2021/02/the-best-javascript-date-libraries/
+
+// 29  février 2020 (mois numéroté à partir de 0) 
+const d = new Date(2020,1,29);
+
+d.toString()
+// 'Sat Feb 29 2020 00:00:00 GMT+0100 (heure normale d’Europe centrale)'
+
+d.getDay() // 6 : jour de la semaine
+d.getDate() // 29 : jour du mois
+d.getFullYear() // 2020 : année sur 4 chiffres
+d.getYear() // 120 : année sur "2" chiffres ;)
+d.getMonth() // 1 : mois mais numéroté à partir de 0
+
+// Le nombre de millisecondes écoulées depuis le premier janvier 1970 à minuit UTC.
+Date.now() // 1645875014577
+
+// obtenir la date du jour !!!
+d.setTime(Date.now())
